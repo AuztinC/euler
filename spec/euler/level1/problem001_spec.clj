aceac398b6fed62e87081e67634e3e28652e44f8
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-04-04 15:18:08 -0400

    problem 02

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -1,15 +1,21 @@
 (ns euler.level1.problem001-spec
-  (:use
-    [speclj.core]
-    [euler.level1.problem001]))
+  (:require [euler.level1.problem001 :refer :all]
+            [speclj.core :refer :all])
+  )
 
 ; http://projecteuler.net/index.php?section=problems&id=1
 
 (describe "Euler Problem #1"
-
+;
   (it "Solves #1"
+    ;(should= 3 (euler-1 3))
+    ;(should= 8 (euler-1 5))
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 233168 (euler-1 1000))
+    )
+
+
+
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,16 @@
 (ns euler.level1.problem001)
 
+; (mod 5 0) => remainder of 5 / 0 = ERROR
+; (mod 5 1) => remainder of 5 / 1 = 0
+; (mod 5 2) => remainder of 5 / 2 = 1
+; (mod 5 3) => remainder of 5 / 3 = 2
+; (mod 5 4) => remainder of 5 / 4 = 1
+; (mod 5 5) => remainder of 5 / 5 = 0
+
+; m is divisible by n if (mod m n) is 0
+; m is divisible by 3 id (mod m 3) is 0
+
 (defn euler-1 [n]
-  )
+  (->> (range n)
+       (filter (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5)))))
+       (reduce +)))
