cdfd9e77364a28f87cfdbcabe0a5b16e741a62aa
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-04-15 15:54:29 -0400

    refactor

--- spec/euler/level1/problem004_spec.clj
+++ spec/euler/level1/problem004_spec.clj
@@ -7,49 +7,43 @@
 
 (describe "Euler Problem #4"
 
-  (context "get product"
-    (it "1 x 1"
-      (should= 1 (product [1 1])))
-    (it "9 x 9 = 81"
-      (should= 9801 (product [99 99])))
-    )
-
   (context "split into str coll"
     (it "number to string"
-      (should= ["9" "8" "0" "1"] (product-to-str-coll [99 99])))
+      (should= ["9" "8" "0" "1"] (->product-str [99 99])))
     )
 
   (context "evenly split reversed coll"
-    (it "[9 8 0 1] to [ [98] [01] ]"
-      (should= [["9""8"]["1""0"]] (subvec-str-coll [99 99])))
+    (it "[9 8 0 1] to [ [98] [10] ]"
+      (should= [["9""8"]["1""0"]] (->halves (->product-str [99 99]))))
     ;(it "111 111"111
     ;  (should= [["1""2"]["2""1"]] (subvec-str-coll [111 111])))
     (it "222 222"
-      (should= [["4""9"]["4""8"]] (subvec-str-coll [222 222])))
-    (it "222 222"
-      (should= [["1" "1" "0"] ["9" "8" "8"]] (subvec-str-coll [333 333])))
+      (should= [["4""9"]["4""8"]] (->halves (->product-str [222 222]))))
+    (it "333 333"
+      (should= [["1" "1" "0"] ["9" "8" "8"]] (->halves (->product-str [333 333]))))
     )
 
   (context "do they match?"
     (it "false double digit palindrome"
-      (should= false (match-check [99 99])))
+      (should= false (palindrome-check? [99 99])))
     (it "true double digit palindrome"
-      (should= true (match-check [91 99])))
+      (should= true (palindrome-check? [91 99])))
     )
 
-  #_(context "recursively check for match"
+  (context "max-palindrome-below"
     (it "highest 2 digit"
-      (should= [[91 99] 9009] (recur-match-check [99 99])))
+      (should= 9009  (max-palindrome-below 99)))
     (it "222"
-      (should= [[207 222] 45954] (recur-match-check [222 222])))
-    ;(it "333"
-    ;  (should=  [[273 333] 90909]  (recur-match-check [333 333])))
+      (should= 45954 (max-palindrome-below 222)))
+    (it "333"
+      (should=  95659 (max-palindrome-below 333)))
     (it "777"
-      (should= [[538 539] 289982] (recur-match-check [777 777])))
+      (should= 571175 (max-palindrome-below 777)))
+    (it "999"
+      (should= 906609 (max-palindrome-below 999)))
     )
 
-  (it "999"
-    (should= [[993 913] 906609] (largest-palindrome [999 999])))
+
   )
 
 (run-specs)
--- src/euler/level1/problem004.clj
+++ src/euler/level1/problem004.clj
@@ -1,64 +1,71 @@
 (ns euler.level1.problem004
   (:require [clojure.string :as str]))
 
-#_(
-    A palindromic number reads the same both ways.
-      The largest palindrome made from the product of two
-      2 digit numbers is
-      9009 = 91 x 99.
+; ->something "creates a something"
+; <-something "from something"
+; a->b "converts from a to b
 
-      Find the largest palindrome made from the product of two
-      3 digit numbers.
+(defn ->product-str [coll]
+  (-> (reduce * coll) str (str/split #"")))
 
-      if count of [] is odd, have to move pointers
-      else split at middle, reverse one side and compare.
-   )
+(defn ->halves [str-coll]
+  (let [len (quot (count str-coll) 2)]
+    [(take len str-coll) (take len (reverse str-coll))]))
 
-(defn product [[n1 n2]]
-  (* n1 n2))
+(defn palindrome-check? [coll]
+  (let [subvec-col (-> coll ->product-str ->halves)]
+    (= (first subvec-col) (second subvec-col))))
 
-(defn product-to-str-coll [coll]
-  (str/split (str (product coll)) #""))
 
-(defn subvec-str-coll [num-col]
-  (let [str-col (product-to-str-coll num-col)]
-    (cond
-      (= 5 (count str-col)) [(subvec str-col 0 2)
-       (vec (reverse (subvec str-col  3)))]
-      (= 4 (count str-col)) [(subvec str-col 0 2)
-       (vec (reverse (subvec str-col (count num-col))))]
-      :else [(subvec str-col 0 3)
-             (vec (reverse (subvec str-col 3)))]
-      )
-    ))
+(defn max-palindrome-below [num]
+  (->> (for [x (map inc (range 1 num))
+             y (map inc (range (inc num)))]
+         [(palindrome-check? [x y]) (* x y)])
+       (filter first)
+       sort
+       last
+       last
+       )
 
-(defn match-check [coll]
-  (let [subvec-col (subvec-str-coll coll)]
-    (every? true? (map #(= (first subvec-col) %) subvec-col))))
-
-(defn largest-palindrome [num-col]
-  (loop [i (first num-col)
+  #_(loop [i num
          max-product 0
          best-pair []]
-    (if (< i 100)
+    (if (< i 0)
       [best-pair max-product]
       (let [[best-product best-pair-inner]
             (loop [j i
                    local-max max-product
                    local-pair best-pair]
-              (if (< j 100)
+              (if (< j 0)
                 [local-max local-pair]
-                (let [prod (* i j) match (match-check [i j])]
+                (let [prod (* i j) match (palindrome-check? [i j])]
                   (if (and match (> prod local-max))
                     (recur (dec j) prod [i j])
                     (recur (dec j) local-max local-pair)))))]
         (recur (dec i) best-product best-pair-inner)))))
+#_(loop [i num
+         max-product 0
+         best-pair []]
+    (if (< i 500)
+      [best-pair max-product]
+      (let [[best-product best-pair-inner]
+            (loop [j i
+                   local-max max-product
+                   local-pair best-pair]
+              (if (< j 500)
+                [local-max local-pair]
+                (let [prod (* i j) match (palindrome-check? [i j])]
+                  (if (and match (> prod local-max))
+                    (recur (dec j) prod [i j])
+                    (recur (dec j) local-max local-pair)))))]
+        (recur (dec i) best-product best-pair-inner))))
 
 #_(defn recur-match-check [num-col]
-  (loop [n-col num-col idx true]
-    (if (match-check n-col)
-      [n-col (reduce * n-col)]
-    (if (true? idx)
-       (recur [(- (first n-col) 1) (last n-col)] false)
-       (recur [(first n-col) (- (last n-col) 1)] true) ))))
+    (loop [n-col num-col idx true]
+      (if (match-check n-col)
+        [n-col (reduce * n-col)]
+        (if (true? idx)
+          (recur [(- (first n-col) 1) (last n-col)] false)
+          (recur [(first n-col) (- (last n-col) 1)] true)))))
+
 
